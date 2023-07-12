6e33f17d3c6f3cd62716507cd858526fd7d45d1f
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2023-07-11 19:42:14 -0500

    Part-1

---
+++ src/main/java/net/endeffect/controller/UserController.java
@@ -0,0 +1,21 @@
+package net.endeffect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/create")
+    public String createUser(Model model){
+
+        return "/user/create";
+    }
+
+
+
+}
