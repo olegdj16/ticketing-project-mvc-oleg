ac05fb4983c2ce4ec4b50ff3ead55c2139443f8d
olegdj16 <oleg16@gmail.com>
2025-03-17 10:21:57 -0500

    SKYN-3 - added BaseEntity, Role, User

---
+++ src/main/java/net/endeffect/entity/Role.java
@@ -0,0 +1,16 @@
+package net.endeffect.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+
+    // these field will be discussed with a team
+    private Long id;
+    private String description;
+
+}
---
+++ src/main/java/net/endeffect/entity/User.java
@@ -0,0 +1,46 @@
+package net.endeffect.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.endeffect.enums.Gender;
+
+import java.time.LocalDateTime;
+
+// we did not include
+// @AllArgsContructor annotation because we want to create our own constructor
+@NoArgsConstructor
+@Data
+public class User extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+    public User(
+            Long id,
+            LocalDateTime insertDateTime,
+            Long insertUserId,
+            LocalDateTime lastUpdateDateTime,
+            Long lastUpdateUserId, String firstName,
+            String lastName, String userName,
+            String password,
+            boolean enabled,
+            String phone,
+            Role role,
+            Gender gender) {
+        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.enabled = enabled;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
+}
