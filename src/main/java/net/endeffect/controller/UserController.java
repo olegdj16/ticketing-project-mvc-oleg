e8f043cbd3d7fe95340829ab1fafdd71a2a836d9
olegdj16 <oleg16@gmail.com>
2025-03-17 10:10:05 -0500

    SKYN-3 - adding enums

---
+++ src/main/java/net/endeffect/controller/UserController.java
@@ -0,0 +1,15 @@
+package net.endeffect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/create")
+    public String userCreate() {
+        return "/user/create";
+    }
+}
