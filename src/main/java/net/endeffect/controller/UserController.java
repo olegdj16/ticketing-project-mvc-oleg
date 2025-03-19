af77590bc8f1ab13444a54abe49a1ead49865f78
olegdj16 <oleg16@gmail.com>
2025-03-19 16:06:00 -0500

    SKYN-4 - 031925_1605 updating UserController, fixing a typo, updating create.html

--- src/main/java/net/endeffect/controller/UserController.java
+++ src/main/java/net/endeffect/controller/UserController.java
@@ -2,6 +2,8 @@ package net.endeffect.controller;
 
 import net.endeffect.dto.RoleDto;
 import net.endeffect.dto.UserDto;
+import net.endeffect.service.RoleService;
+import net.endeffect.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    // we need to inject
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String userCreate(Model model) {
 
         model.addAttribute("user", new UserDto());
-
-//        model.addAttribute("role", LIST_OF_ROLES);
+        model.addAttribute("roles", roleService.findAll());
 
 
         return "/user/create";
