7b7baab034d2c50b69dce020a1d96201983f2bbc
olegdj16 <oleg16@gmail.com>
2025-03-19 22:19:00 -0500

    SKYN-4 - 031925_2218 updating insertUser to user redirect

--- src/main/java/net/endeffect/controller/UserController.java
+++ src/main/java/net/endeffect/controller/UserController.java
@@ -34,33 +34,45 @@ public class UserController {
         return "/user/create";
     }
 
-    /**
-     * @PostMapping("/create"): Maps the method to handle POST requests to /user/create.
-     * @ModelAttribute("user") UserDto user: Binds the form data to a UserDto object.
-     * userService.save(user): Saves the user.
-     * model.addAttribute("user", new UserDto()): Adds an empty UserDto object to the model for the form.
-     * model.addAttribute("roles", roleService.findAll()): Adds the list of roles to the model.
-     * model.addAttribute("users", userService.findAll()): Adds the list of users to the model.
-     * return "/user/create": Returns the view name for the user creation form.
-     * @param user
-     * @param model
-     * @return
-     */
+
+
+    // Version 1 of insertUser
+//    /**
+//     * @PostMapping("/create"): Maps the method to handle POST requests to /user/create.
+//     * @ModelAttribute("user") UserDto user: Binds the form data to a UserDto object.
+//     * userService.save(user): Saves the user.
+//     * model.addAttribute("user", new UserDto()): Adds an empty UserDto object to the model for the form.
+//     * model.addAttribute("roles", roleService.findAll()): Adds the list of roles to the model.
+//     * model.addAttribute("users", userService.findAll()): Adds the list of users to the model.
+//     * return "/user/create": Returns the view name for the user creation form.
+//     * @param user
+//     * @param model
+//     * @return
+//     */
+//    @PostMapping("/create")
+//    public String insertUserDeprecate(UserDto user, Model model){
+//
+//        // need this: after save, you need an empty form back. this will provide an empty object
+//        model.addAttribute("user", new UserDto());
+//
+//        // The line model.addAttribute("roles", roleService.findAll()); is necessary to populate the roles attribute in the
+//        // model with a list of all roles retrieved from the RoleService. This list is then used in the Thymeleaf template to
+//        // display the available roles in a dropdown menu for the user to select from when creating a new user.
+//        model.addAttribute("roles", roleService.findAll());
+//
+//        userService.save(user);
+//        model.addAttribute("users", userService.findAll());
+//
+//        return "/user/create";
+//    }
+
+    // Version 2 of insertUser using redirect
     @PostMapping("/create")
     public String insertUser(UserDto user, Model model){
 
-        // need this: after save, you need an empty form back. this will provide an empty object
-        model.addAttribute("user", new UserDto());
-
-        // The line model.addAttribute("roles", roleService.findAll()); is necessary to populate the roles attribute in the
-        // model with a list of all roles retrieved from the RoleService. This list is then used in the Thymeleaf template to
-        // display the available roles in a dropdown menu for the user to select from when creating a new user.
-        model.addAttribute("roles", roleService.findAll());
-
         userService.save(user);
-        model.addAttribute("users", userService.findAll());
 
-        return "/user/create";
+        return "redirect:/user/create";
     }
 
 }
